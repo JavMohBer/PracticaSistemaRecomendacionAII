dd1f21f084dc9d3cd05364e9161f1256476893ca
Javier Parra <javierparraoyola619@hotmail.com>
2017-12-21 01:30:49 +0100

    Añadidos archivos y modificación de otros

--- SistemaRecomendacion/principal/models.py
+++ SistemaRecomendacion/principal/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.core.validators import URLValidator
 
 # Create your models here.
+
+class Occupation(models.Model):
+    occupationName = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.occupationName
+
+
+class Genre(models.Model):
+    genreName = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.genreName
+
+
+class UserInformation(models.Model):
+    gender = models.CharField(max_length=1, choices=(('F', 'Female'), ('M', 'Male'),))
+    occupation = models.ForeignKey(Occupation, on_delete=models.DO_NOTHING)
+    zipCode = models.CharField(max_length=8)
+
+    def __unicode__(self):
+        return self.gender + self.zipCode
+
+
+class Film(models.Model):
+    movieTitle = models.CharField(max_length=100)
+    releaseDate = models.DateField(null=True, blank=True)
+    releaseVideoDate = models.DateField(null=True, blank=True)
+    IMDbURL = models.URLField(validators=[URLValidator()])
+    genres = models.ManyToManyField(Genre)
+    ratings = models.ManyToManyField(UserInformation, through="Rating")
+
+    def __unicode__(self):
+        return self.movieTitle
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(UserInformation)
+    film = models.ForeignKey(Film)
+    rateDate = models.DateField(null=True, blank=True)
--- SistemaRecomendacion/principal/views.py
+++ SistemaRecomendacion/principal/views.py
@@ -1,3 +1,105 @@
 from django.shortcuts import render
+import shelve
+from SistemaRecomendacion.principal.models import UserInformation, Film, Rating
+from SistemaRecomendacion.principal.forms import UserForm, FilmForm
+from django.template import RequestContext
+from django.shortcuts import render_to_response, get_object_or_404
+from SistemaRecomendacion.principal.recommendations import transformPrefs, calculateSimilarItems, getRecommendedItems, topMatches
+from SistemaRecomendacion.principal.populate import populateDatabase
 
 # Create your views here.
+
+Prefs = {}  # matriz de usuarios y puntuaciones a cada a items
+ItemsPrefs = {}  # matriz de items y puntuaciones de cada usuario. Inversa de Prefs
+SimItems = []  # matriz de similitudes entre los items
+
+
+# Funcion que carga en el diccionario Prefs todas las puntuaciones de usuarios a peliculas. Tambien carga el diccionario inverso y la matriz de similitud entre items
+# Serializa los resultados en dataRS.dat
+def loadDict():
+    shelf = shelve.open("dataRS.dat")
+    ratings = Rating.objects.all()
+    for ra in ratings:
+        user = int(ra.user.id)
+        itemid = int(ra.film.id)
+        rating = float(ra.rating)
+        Prefs.setdefault(user, {})
+        Prefs[user][itemid] = rating
+    shelf['Prefs'] = Prefs
+    shelf['ItemsPrefs'] = transformPrefs(Prefs)
+    shelf['SimItems'] = calculateSimilarItems(Prefs, n=10)
+    shelf.close()
+
+
+#  CONJUNTO DE VISTAS
+
+def index(request):
+    return render_to_response('index.html')
+
+
+def populateDB(request):
+    populateDatabase()
+    return render_to_response('populate.html')
+
+
+def loadRS(request):
+    loadDict()
+    return render_to_response('loadRS.html')
+
+
+# APARTADO A
+def search(request):
+    if request.method == 'GET':
+        form = UserForm(request.GET, request.FILES)
+        if form.is_valid():
+            idUser = form.cleaned_data['id']
+            user = get_object_or_404(UserInformation, pk=idUser)
+            return render_to_response('ratedFilms.html', {'usuario': user})
+    else:
+        form = UserForm()
+    return render_to_response('search_user.html', {'form': form}, context_instance=RequestContext(request))
+
+
+# APARTADO B
+def recommendedFilms(request):
+    if request.method == 'GET':
+        form = UserForm(request.GET, request.FILES)
+        if form.is_valid():
+            idUser = form.cleaned_data['id']
+            user = get_object_or_404(UserInformation, pk=idUser)
+            shelf = shelve.open("dataRS.dat")
+            Prefs = shelf['Prefs']
+            SimItems = shelf['SimItems']
+            shelf.close()
+            rankings = getRecommendedItems(Prefs, SimItems, int(idUser))
+            recommended = rankings[:2]
+            items = []
+            for re in recommended:
+                item = Film.objects.get(pk=re[1])
+                items.append(item)
+            return render_to_response('recommendationItems.html', {'user': user, 'items': items},
+                                      context_instance=RequestContext(request))
+    form = UserForm()
+    return render_to_response('search_user.html', {'form': form}, context_instance=RequestContext(request))
+
+
+# APARTADO C
+def similarFilms(request):
+    film = None
+    if request.method == 'GET':
+        form = FilmForm(request.GET, request.FILES)
+        if form.is_valid():
+            idFilm = form.cleaned_data['id']
+            film = get_object_or_404(Film, pk=idFilm)
+            shelf = shelve.open("dataRS.dat")
+            ItemsPrefs = shelf['ItemsPrefs']
+            shelf.close()
+            recommended = topMatches(ItemsPrefs, int(idFilm), n=3)
+            items = []
+            for re in recommended:
+                item = Film.objects.get(pk=int(re[1]))
+                items.append(item)
+            return render_to_response('similarFilms.html', {'film': film, 'films': items},
+                                      context_instance=RequestContext(request))
+    form = FilmForm()
+    return render_to_response('search_film.html', {'form': form}, context_instance=RequestContext(request))
